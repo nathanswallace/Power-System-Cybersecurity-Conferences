123c967d699c3dfa6235e3cb4faac3b1f87529bd
Nathan <nathanwallace@computer.org>
2018-07-18 18:37:13 -0500

    Several updates.

--- HTML_Generator/main.py
+++ HTML_Generator/main.py
@@ -3,6 +3,7 @@
 #==============================================================================
 # HTML Generator Main
 # Author: @NathanSWallace
+# This file examines all CSV files and updates the HTML tables accordingly
 #==============================================================================
 import os
 import csv
@@ -15,12 +16,31 @@ def getpath(filename):
     filepath = os.path.abspath(os.path.join(path, filename))
     return filepath
 
+#Function to copy and modify event template file, event.html, and save off the new one.
+#def SetupEventPage(EventID):
+
 def EventsUpdate():
     insertTable = ''
     with open(getpath("CSV_Files/events.csv"), 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            insertTable += '<tr class="item"><td><a href="%s" target="_blank">%s</a></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n' % (row['Link'],row['Name'],row['Started'],row['Type'],row['Category'],row['Published'],row['Recorded'])
+            #If EventFolder isnt created need to create both CSV and HTML directories.
+            #1st CSV directory, to store CSV of papers, talks etc for each
+            if not os.path.exists(getpath("CSV_Files/"+row["EventID"])):
+                os.makedirs(getpath("CSV_Files/"+row["EventID"]))
+            #2nd Events directory, to store HTML code for each EventID.
+            if not os.path.exists(getpath("../events/"+row["EventID"])):
+                os.makedirs(getpath("../events/"+row["EventID"]))
+            #Adds the row to the html table temp insert variable
+            link = row['Link']
+            ####### TO DO - Come back and adjust target. Open extenal link in new tab. Open internal link in same window.
+            #Check to see if the app will contain an internal link to the event page or redirect to the main event's website.
+            if row['InternalLink'] == 'Yes':
+                link = getpath("../events/"+row["EventID"]+'/')
+                #SetupEventPage(row['EventID'])
+            insertTable += '<tr class="item"><td><a href="%s" target="_blank">%s</a></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n' % (link,row['Name'],row['Started'],row['Type'],row['Category'],row['Published'],row['Recorded'])
+
+    #Copies insertTable into the proper place in the index.html file identified by [tag_events]
     with open(getpath("HTML_Templates/index.html"), 'r') as indexTemp:
         with open("index.html", "w") as fout:
             for line in indexTemp:
--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# Power System Cybersecurity Conferences & Papers 
+# Power System Cybersecurity Conferences & Papers
 
 This project was created to help shed light on the number of conferences and people that are working tirelessly to research & develop solutions and create domain expertise focused on securing power system environments. While there are a number of on general industrial control system (ICS) cybersecurity, there are a select few that truly focus on the solutions and challenges of securing power systems.
 
 ## Getting Started
 
-Go to..
-
+To create or update HTML files run 'python3 HTML_Generator/main.py' in the base directory of the app.
 
 ## Contributing
 
@@ -13,7 +12,7 @@ Details coming soon.
 
 ## License
 
-This project is licensed under the GNU GENERAL PUBLIC v3 License - see the [LICENSE.md](LICENSE.md) file for details
+Details coming soon.
 
 ## Acknowledgments
 
